40d2c8502ad067f527490c1654fd5445f0a23e97
Віктор Жук <160972371+zazplay@users.noreply.github.com>
2025-02-03 23:27:12 +0200

    Upd imorts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import * as crypto from 'crypto';
-(global as any).crypto = crypto;
+
 
 
 async function bootstrap() {
