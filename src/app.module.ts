678663e6af6d9cfaf411ac3e4222fa8989b6bdf6
Віктор Жук <160972371+zazplay@users.noreply.github.com>
2025-02-03 23:39:52 +0200

    Update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import * as crypto from 'crypto'; 
 
 @Module({
   imports: [
