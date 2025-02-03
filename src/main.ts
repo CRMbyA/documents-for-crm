1dd39038769e1d2549f4b2c33444e5bf42b9b925
Віктор Жук <160972371+zazplay@users.noreply.github.com>
2025-02-04 00:08:59 +0200

    Add filter by tag

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,30 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   process.env.TZ = 'Europe/Kiev';
-
+  
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   app.enableCors();
-
+  
   const config = new DocumentBuilder()
     .setTitle('CRM крупные базы')
-    .setDescription('API для управления CRM базами данных') 
+    .setDescription('API для управления CRM базами данных')
     .setVersion('2.0')
     .build();
-
+    
   const document = SwaggerModule.createDocument(app, config, {
     deepScanRoutes: true
   });
   
-  SwaggerModule.setup('api/docs', app, document);
+  const customOptions = {
+    swaggerOptions: {
+      filter: true
+    }
+  };
 
+  SwaggerModule.setup('api/docs', app, document, customOptions);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
